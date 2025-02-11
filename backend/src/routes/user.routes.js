563a484a40240f4d178b642a2f96aab5fccaae9c
Bhavik Vimal Prajapati <bhavikp0026@gmail.com>
2025-02-12 00:15:36 +0530

    Add order model and controller; implement user routes for password change and current user retrieval

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -4,6 +4,8 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
 } from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
@@ -17,4 +19,10 @@ router.route("/login").post(upload.none(), loginUser);
 //secured Routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router
+  .route("/change-password")
+  .put(upload.none(), verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(getCurrentUser);
+
 export default router;
