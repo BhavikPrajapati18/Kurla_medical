86bef43dd756fc956ee430ecd669c168b08a6834
Bhavik Vimal Prajapati <bhavikp0026@gmail.com>
2025-02-25 13:06:06 +0530

    Refactor store configuration for improved readability and consistency

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authSlice from './authSlice'
-import cartSlice from './cartSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import cartSlice from "./cartSlice";
 
-  const store = configureStore({
-    reducer : {
-      auth : authSlice,
-      user: cartSlice,
-    }
-  });
+const initialState = {};
 
-export default store
+const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    user: cartSlice,
+  },
+});
+
+export default store;
